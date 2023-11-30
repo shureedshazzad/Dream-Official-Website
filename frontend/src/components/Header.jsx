8a3b267484a735e09eb9bb28067216bf5f0acfda
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-11-30 13:27:34 +0600

    change header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,35 +1,52 @@
-import React from 'react'
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import pic1 from './header_img/pic1.jpg';
 import pic2 from './header_img/pic2.jpg';
-import pic3 from './header_img/pic3.jpg';
-
-
+import { faItalic } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
   return (
     <div className="container-fluid header bg-danger p-0 mb-5">
-        <div className="row g-0 align-items-center flex-column-reverse flex-lg-row">
-            <div className="col-lg-6 p-5 wow fadeIn" data-wow-delay="0.1s">
-                <h1 className="display-10 text-white mb-5 font-italic">At Dream Blood Donation Club, we're dedicated to saving lives through voluntary blood donation. Join us in making a meaningful impact, one pint at a time.</h1>
-                <div className="row g-4">
-                </div>
-            </div>
-            <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-                <div className="owl-carousel header-carousel">
-                    <div className="owl-carousel-item position-relative">
-                        <img className="img-fluid" src={pic1} alt=""/>
-                    </div>
-                    <div className="owl-carousel-item position-relative">
-                        <img className="img-fluid" src={pic2} alt=""/>
-                    </div>
-                    <div className="owl-carousel-item position-relative">
-                        <img className="img-fluid" src={pic3} alt=""/>
-                    </div>
-                </div>
-            </div>
+      <div className="row g-0 align-items-center flex-column-reverse flex-lg-row">
+        {/* Text Column */}
+        <div className="col-lg-6 p-5 wow fadeIn" data-wow-delay="0.1s">
+          <h1 className="display-10 text-white mb-5" style={{fontStyle: 'italic', fontSize: '30px'}}>At Dream Blood Donation Club, our commitment revolves around the noble cause of preserving lives through voluntary blood donation. We invite you to join hands with us in creating a profound difference, one pint at a time. Your participation can be the lifeline that makes a lasting impact on the well-being of those in need</h1>
+          <div className="row g-3">
+            {/* Additional content if needed */}
+          </div>
         </div>
+
+        {/* Image Slider Column */}
+        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+          <div className="slider-container" style={{marginLeft: 200}}>
+            <Slider {...settings}>
+              <div>
+                <img className="img-fluid" src={pic1} alt="Header Image 1" />
+              </div>
+              <div>
+                <img className="img-fluid" src={pic2} alt="Header Image 2" />
+              </div>
+              {/*<div>
+                <img className="img-fluid" src={pic1} alt="Header Image 3" />
+              </div>*/}
+            </Slider>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
