afb96ed225a8a3ddf0679d18bdb9199d40fbf9f1
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-12-01 23:05:20 +0600

    fix button issue

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,46 +1,58 @@
-import React from 'react';
+import React from 'react'
 
-const footerStyle = {
-  backgroundColor: '#343a40', // Dark background color
-  color: '#fff',             // Text color
-  padding: '40px 0',         // Padding top and bottom
+const footerWidth = {
+    maxWidth: '400px',
 };
+const logoStyle = {
+    height: "40px",
+    width: "40px"
+  };
 
-const addressStyle = {
-  marginBottom: '20px',       // Margin bottom for the address
-};
-
-const copyrightStyle = {
-  borderTop: '1px solid rgba(255, 255, 255, 0.2)', // Border color
-  paddingTop: '20px',                              // Padding top
-};
-
-const Footer = () => {
+function Footer() {
   return (
-    <div className="container-fluid" style={footerStyle}>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 text-center" style={addressStyle}>
-            <p>
-              Address: Khulna University of Engineering & Technology, Khulna, Bangladesh | Phone: +123 456 789 | Email: info@example.com
-            </p>
-          </div>
+    <div className="container-fluid bg-dark text-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div className="container py-5">
+            <div className="row g-5">
+                <div className="col-lg-3 col-md-4">
+                    <h5 className="text-light mb-4">Address</h5>
+                    <p className="mb-2"><i className="fa fa-map-marker-alt me-3"></i>Khulna University of Engineering & Technology, Khulna</p>
+                    <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>+012 345 67890</p>
+                    <p className="mb-2"><i className="fa fa-envelope me-3"></i>info@example.com</p>
+                    <div className="d-flex pt-2">
+                        <a className="btn btn-outline-light btn-social rounded-circle" href=""><i className="fab fa-twitter"></i></a>
+                        <a className="btn btn-outline-light btn-social rounded-circle" href=""><i className="fab fa-facebook-f"></i></a>
+                        <a className="btn btn-outline-light btn-social rounded-circle" href=""><i className="fab fa-youtube"></i></a>
+                        <a className="btn btn-outline-light btn-social rounded-circle" href=""><i className="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div className="col-lg-9 col-md-6" style={{marginTop: "100px"}}>
+                At Dream Blood Donation Club of KUET, our commitment revolves around the noble cause of preserving lives through voluntary blood donation. We invite you to join hands with us in creating a profound difference, one pint at a time. Your participation can be the lifeline that makes a lasting impact on the well-being of those in need
+                <p style={{marginTop: "5px"}}>
+                    Powered by: 
+                </p>
+                <p>
+                <img src="/images/logo.png" alt="Logo" className="mr-2" style={logoStyle}/>
+                <img src="/images/kuetLogo.png" alt="Logo" className="mr-2" style={{ ...logoStyle, marginLeft: "15px" }}
+/>
+                </p>
+                </div>
+            </div>
         </div>
-        <div className="row">
-          {/* Additional content or columns can be added here */}
+        <div className="container">
+            <div className="copyright">
+                <div className="row">
+                    <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a href="#">dream.com</a>, All Right Reserved.
+                    </div>
+                    <div className="col-md-6 text-center text-md-end">
+                        Designed By <a href="https://github.com/shureedshazzad" style={{ color: '#3498db', textDecoration: 'underline' }}>Shureed Shazzad</a> &
+                <a href="https://github.com/Sk-Azraf-Sami" style={{ color: '#3498db', textDecoration: 'underline' }}> Azraf Sami</a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="row">
-          <div className="col-md-12 text-center" style={copyrightStyle}>
-            <p>
-              &copy; 2023 DREAM.com. All Rights Reserved. | Designed By <a href="https://github.com/shureedshazzad" style={{ color: '#3498db', textDecoration: 'underline' }}>Shureed Shazzad</a> &
-              <a href="https://github.com/Sk-Azraf-Sami" style={{ color: '#3498db', textDecoration: 'underline' }}> Azraf Sami</a>
-            
-            </p>
-          </div>
-        </div>
-      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
