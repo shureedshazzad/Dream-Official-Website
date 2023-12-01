8695b490a337f4fcdcd7fd891d90d4bc59825fb9
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-12-02 02:05:43 +0600

    add kmc information page

--- frontend/src/screens/Adminscreen/Eventinfo.jsx
+++ frontend/src/screens/Adminscreen/Eventinfo.jsx
@@ -1,20 +1,17 @@
-
 import React, { useState } from 'react';
-import { useGetEventsQuery,useDeleteEventMutation } from '../../slices/eventsApiSlice';
+import { useGetEventsQuery, useDeleteEventMutation } from '../../slices/eventsApiSlice';
 import Loader from '../../components/Loader';
 import { LinkContainer } from 'react-router-bootstrap';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
 
 function Eventinfo() {
-  const { data: events, refetch,isLoading: eventsLoading, error: eventsError } = useGetEventsQuery();
+  const { data: events, refetch, isLoading: eventsLoading, error: eventsError } = useGetEventsQuery();
 
-  const[deleteEvent, {isLoading: loadingDelete}] = useDeleteEventMutation();
+  const [deleteEvent, { isLoading: loadingDelete }] = useDeleteEventMutation();
 
-  const deleteHandler = async(id) => {
-
-
-    if(window.confirm("Are you sure you want to delete the event?")){
+  const deleteHandler = async (id) => {
+    if (window.confirm('Are you sure you want to delete the event?')) {
       try {
         await deleteEvent(id);
         toast.success('Event is deleted');
@@ -23,8 +20,7 @@ function Eventinfo() {
         toast.error(err?.data?.message || err.error);
       }
     }
-  }
-
+  };
 
   if (eventsLoading) {
     return <Loader />;
@@ -34,50 +30,50 @@ function Eventinfo() {
     return <div>Error loading events</div>;
   }
 
-  //Filter out events with endDate passed
-  const filteredEvents = events.filter((event) => new Date(event. availableday.endDate) > new Date());
+  // Filter out events with endDate passed
+  const filteredEvents = events.filter((event) => new Date(event.availableday.endDate) > new Date());
 
-  //   Sort events based on startDate
-   const sortedEvents = filteredEvents.sort((a, b) => new Date(a.availableday.startDate) - new Date(b.availableday.startDate));
+  // Sort events based on startDate
+  const sortedEvents = filteredEvents.sort((a, b) => new Date(a.availableday.startDate) - new Date(b.availableday.startDate));
 
   return (
-    <div className="container py-5">
-    {sortedEvents.length === 0 ? (
-      <p>No events available.</p>
-    ) : (
-      <>
-        <div className="row g-4">
-          {sortedEvents.map((event) => (
-            <div key={event.id} className="col-md-6 col-lg-4">
-              <div className="card h-100 wow fadeInUp" data-wow-delay="0.1s">
-                <div className="card-body bg-gray">
-                  <h5 className="card-title">{event.heading}</h5>
-                  <p className="card-text">{event.description.substring(0, 100)}...</p>
-                  <p>
-                    <strong>Date Range:</strong>{' '}
-                    {new Date(event.availableday.startDate).toLocaleDateString()} - {new Date(event.availableday.endDate).toLocaleDateString()}
-                  </p>
-                </div>
-                <div className="card-footer d-flex justify-content-between">
-                  <LinkContainer to={`/admin/event/all/${event._id}`}>
-                    <button className="btn btn-outline-primary">Read Details</button>
-                  </LinkContainer>
-                  <button className="btn btn-outline-danger" onClick={() => deleteHandler(event._id)}>
-                    <faTrash /> Delete
-                  </button>
+    <div className="container py-5 position-relative">
+      {sortedEvents.length === 0 ? (
+        <p>No events available.</p>
+      ) : (
+        <>
+          <div className="row g-4">
+            {sortedEvents.map((event) => (
+              <div key={event.id} className="col-md-6 col-lg-4">
+                <div className="card h-100 wow fadeInUp" data-wow-delay="0.1s">
+                  <div className="card-body bg-gray">
+                    <h5 className="card-title">{event.heading}</h5>
+                    <p className="card-text">{event.description.substring(0, 100)}...</p>
+                    <p>
+                      <strong>Date Range:</strong>{' '}
+                      {new Date(event.availableday.startDate).toLocaleDateString()} - {new Date(event.availableday.endDate).toLocaleDateString()}
+                    </p>
+                  </div>
+                  <div className="card-footer d-flex justify-content-between">
+                    <LinkContainer to={`/admin/event/all/${event._id}`}>
+                      <button className="btn btn-outline-primary">Read Details</button>
+                    </LinkContainer>
+                    <button className="btn btn-outline-danger" onClick={() => deleteHandler(event._id)}>
+                      <faTrash /> Delete
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <div className="fixed-bottom m-1 text-end">
-          <LinkContainer to="/admin/event/create">
-            <button className="btn btn-danger">Create Event</button>
-          </LinkContainer>
-        </div>
-      </>
-    )}
-  </div>
+            ))}
+          </div>
+          <div className="position-absolute top-0 end-0 m-3">
+            <LinkContainer to="/admin/event/create">
+              <button className="btn btn-danger">Create Event</button>
+            </LinkContainer>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
---
+++ frontend/src/screens/MedicalCenterInfo.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserDoctor, faBed, faPhone, faUserNurse, faClock } from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { Button } from 'react-bootstrap';
+
+const MedicalCenterInfo = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center">Kuet Medical Center</h1>
+      <hr />
+      <div className="row">
+        <div className="col-sm-8">
+          <iframe
+            title="Google Map"
+            src="https://www.google.com/maps/embed/v1/place?q=KUET+Medical+Center,+KUET+Road,+Khulna,+Bangladesh&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            width="100%"
+            height="320"
+            frameBorder="0"
+            style={{ border: '0' }}
+            allowFullScreen
+          ></iframe>
+        </div>
+
+        <div className="col-sm-4" id="contact2">
+          <h3>Information</h3>
+          <hr align="left" width="50%" />
+          
+          <h5 className="pt-2"> <FontAwesomeIcon icon={faBed} style={{ color: '#000' }} /> Number of Beds: <small style={{color: ""}}>50</small></h5>
+          <h5 className="pt-2"> <FontAwesomeIcon icon={faUserDoctor} style={{ color: '#000' }} /> Number of Doctors: <small style={{color: ""}}>5</small></h5>
+          <h5 className="pt-2"> <FontAwesomeIcon icon={faUserNurse} style={{ color: '#000' }} /> Number of Nurses: <small style={{color: ""}}>3</small></h5>
+          <h5 className="pt-2"> <FontAwesomeIcon icon={faClock} style={{ color: '#000' }} /> Opening Hours: <small style={{color: ""}}>9:00 AM - 5:00 PM</small></h5>
+         <br />
+        </div>
+      </div>
+
+      <br /><br />
+      
+        <div>
+         <Button className='row bg-danger' style={{ width: '100%', fontWeight: "bold" }}>Doctor Appointments</Button>
+        </div>
+        
+      </div>
+  
+  );
+};
+
+export default MedicalCenterInfo;
